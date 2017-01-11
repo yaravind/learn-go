d7147e5a3c1391e37ea4ee77aac0daf06c42d673
Aravind R Yarram <yaravind@gmail.com>
2017-01-11 11:44:36 -0500

    Convert a goroutine to be a generator

--- concurrency/main.go
+++ concurrency/main.go
@@ -10,19 +10,44 @@ import (
 )
 
 func main() {
+
+	histogram := make(map[string]int)
+	bytesCh := textGen("c:/aravind/resume.txt")
+
+	for {
+		if bytes, open := <-bytesCh; open {
+			words := strings.Split(string(bytes), " ")
+			for _, word := range words {
+				word = strings.ToLower(word)
+				histogram[word]++
+			}
+		} else {
+			break
+		}
+	}
+
+	for k, v := range histogram {
+		fmt.Printf("%s\t(%d)\n", k, v)
+	}
+}
+
+func textGen(filePath string) <-chan []byte {
 	nBytes, nChunks := int64(0), int64(0)
 
-	f, err := os.Open("c:/aravind/resume.txt")
+	f, err := os.Open(filePath)
 	if err != nil {
 		log.Fatal(err)
+		defer f.Close()
 	}
 	r := bufio.NewReader(f)
+
 	buf := make([]byte, 0, 1024)
 	log.Println("opend reader for the file")
-	textCh := make(chan []byte, 2)
 
+	textCh := make(chan []byte, 2)
 	go func() {
 		defer close(textCh) //close before returning from goroutine
+
 		for {
 			n, err := r.Read(buf[:cap(buf)])
 			buf = buf[:n]
@@ -44,24 +69,7 @@ func main() {
 				log.Fatal(err)
 			}
 		}
-		log.Printf("done from goroutine len=%d\n", len(textCh))
 	}()
-
-	histogram := make(map[string]int)
-	for {
-		if bytes, open := <-textCh; open {
-			words := strings.Split(string(bytes), " ")
-			log.Printf("waiting for data on channel len=%d", len(textCh))
-			for _, word := range words {
-				word = strings.ToLower(word)
-				histogram[word]++
-			}
-		} else {
-			break
-		}
-	}
-
-	for k, v := range histogram {
-		fmt.Printf("%s\t(%d)\n", k, v)
-	}
+	log.Printf("done from goroutine len=%d\n", len(textCh))
+	return textCh
 }
